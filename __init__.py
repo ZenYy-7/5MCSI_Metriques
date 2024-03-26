83f3e99f47233381287588611de45a2dea2ac0fd
ZenYy-7 <149468894+ZenYy-7@users.noreply.github.com>
2024-03-26 10:46:01 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-@app.route("/contact/")
+@app.route("/contact1/")
 def MaPremiereAPI(): 
     return render_template("contact1.html")
 
